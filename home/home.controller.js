bc9f658ae24301340063446f61d0691a089e8595
Long Nguyen Thanh <long.nguyenthanh@axonactive.com>
2018-01-12 09:37:45 +0700

    showed price

--- home/home.controller.js
+++ home/home.controller.js
@@ -5,18 +5,22 @@
             .controller('HomeController', HomeController);
     HomeController.$inject = ['$rootScope', '$scope', '$http'];
     function HomeController($rootScope, $scope, $http) {      
-        $rootScope.title = "shenlong-webutl";
-        $scope.initMap = initMap;
+        $rootScope.title = "shenlong-webutl";        
         $scope.map = {};
+        $scope.parkingMarkers = [];
         $scope.loadParkingInBoudingMap = loadParkingInBoudingMap;
         $scope.getParkingInBoundingMap = getParkingInBoundingMap;
         $scope.processData = processData;
+        $scope.initMap = initMap;
+        $scope.clearCurrentParkingMarkers = clearCurrentParkingMarkers;
         initController();
         function initController() {
            
         }
         function initMap(map){
             $scope.map = map;
+            $scope.map.setCenter(new google.maps.LatLng(47.613701, -122.350535));
+            
             google.maps.event.addListener($scope.map, 'idle', function(){
                 loadParkingInBoudingMap();                          
             });
@@ -41,7 +45,44 @@
             });
         }
         function processData(data){
-            
+            clearCurrentParkingMarkers();
+            $.each(data.result, function(i, e){
+
+                var prices = '';
+                $.each(e.rateCard, function(cri, cre){
+                    prices += '<p>'+cre+'</p>'
+                });
+               
+
+                var contentString = 
+                    '<div id="content">'+
+                        '<div id="siteNotice">'+'</div>'+
+                        '<h1 id="firstHeading" class="firstHeading">Price</h1>'+
+                        '<div id="bodyContent">'+ 
+                            prices + 
+                        '</div>'+
+                    '</div>';
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });  
+
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(e.point.coordinates[1],e.point.coordinates[0]),
+                    map: $scope.map,
+                    title: e.name + " - " + e.operator
+                  });
+                  marker.addListener('click', function() {
+                    infowindow.open($scope.map, marker);
+                  });
+                  $scope.parkingMarkers.push(marker);
+            });
+        }
+        function clearCurrentParkingMarkers(){
+            $.each($scope.parkingMarkers, function(i, e){
+                e.setMap(null);                
+            });
+            $scope.parkingMarkers = [];
         }
     }
 
